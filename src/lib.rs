cb5b6866505c24651dccf1570199f07980f8cad6
Codadillo <leoconr@nuevaschool.org>
2021-02-15 18:48:56 -0800

    Added executor

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,26 @@
 mod tests;
 
 use serde::de::DeserializeOwned;
-use std::{any::Any, collections::HashMap};
+use std::{collections::HashMap, marker::PhantomData};
 use unicode_segmentation::UnicodeSegmentation;
 
 pub trait CallParsed<State, T> {
     fn call_parsed(&mut self, state: &mut State, args: String) -> Result<(), serde_json::Error>;
+}
+
+trait CallParsedErased<State> {
+    fn call_parsed(&mut self, state: &mut State, args: String) -> Result<(), serde_json::Error>;
+}
+
+struct Wrapper<State, T, C> {
+    data: C,
+    phantom: PhantomData<(State, T)>,
+}
+
+impl<State, T, C: CallParsed<State, T>> CallParsedErased<State> for Wrapper<State, T, C> {
+    fn call_parsed(&mut self, state: &mut State, args: String) -> Result<(), serde_json::Error> {
+        self.data.call_parsed(state, args)
+    }
 }
 
 macro_rules! call_parsed_impls {
@@ -18,10 +33,10 @@ macro_rules! call_parsed_impls {
             $( $tail: DeserializeOwned ),*
 
         {
-            fn call_parsed(&mut self, state8348912731: &mut EUCBNAJHXIZAD81923IX, args: String) -> Result<(), serde_json::Error> {
+            fn call_parsed(&mut self, state8348912731: &mut EUCBNAJHXIZAD81923IX, args: String) -> Result<(), serde_json::Error> {              
                 let mut graphemes: Vec<&str> = args.as_str().graphemes(true).collect();
-                *graphemes.first_mut().unwrap() = "[";
-                *graphemes.last_mut().unwrap() = "]";
+                graphemes.first_mut().map(|g| *g = "[");
+                graphemes.last_mut().map(|g| *g = "]");
 
                 #[allow(non_snake_case)]
                 let ($head, $( $tail ),*): ($head, $( $tail ),*) = serde_json::from_str(&graphemes.into_iter().collect::<String>())?;
@@ -30,6 +45,7 @@ macro_rules! call_parsed_impls {
                 Ok(())
             }
         }
+
         call_parsed_impls!($( $tail, )*);
     };
     () => {
@@ -53,7 +69,11 @@ pub struct LSystem {
 }
 
 impl LSystem {
-    pub fn new(
+    pub fn new(axiom: String) -> Self {
+        Self::with_rules(axiom, HashMap::new())
+    }
+
+    pub fn with_rules(
         axiom: String,
         production_rules: HashMap<String, Box<dyn FnMut() -> String>>,
     ) -> Self {
@@ -107,28 +127,62 @@ impl LSystem {
 }
 
 pub struct LSystemExecutor<State> {
-    execution_rules: HashMap<String, Box<dyn Any>>,
-    state: State,
+    pub state: State,
+    execution_rules: Vec<(String, Box<dyn CallParsedErased<State>>)>,
 }
 
-impl<State> LSystemExecutor<State> {
+impl<State: 'static> LSystemExecutor<State> {
     pub fn new(state: State) -> Self {
         Self {
-            execution_rules: HashMap::new(),
+            execution_rules: vec![],
             state,
         }
     }
 
-    pub fn state_mut(&mut self) -> &mut State {
-        &mut self.state
-    }
-
-    pub fn register_execution_rule<T>(
+    pub fn register_execution_rule<T: 'static>(
         &mut self,
         token: String,
         rule: impl 'static + CallParsed<State, T>,
     ) {
-        self.execution_rules
-            .insert(token, Box::new(rule) as Box<dyn Any>);
+        self.execution_rules.push((
+            token,
+            Box::new(Wrapper {
+                data: rule,
+                phantom: PhantomData,
+            }) as Box<dyn CallParsedErased<State>>,
+        ));
+    }
+
+    pub fn execute(&mut self, system: &LSystem) -> Result<(), serde_json::Error> {
+        let mut instructions = system.axiom.clone();
+
+        while instructions.len() != 0 {
+            if let Some((token, rule)) = self
+                .execution_rules
+                .iter_mut()
+                .find(|e| instructions.starts_with(&e.0))
+            {
+                let _: String = instructions.drain(..token.len()).collect();
+                if instructions.chars().next() != Some('(') {
+                    rule.call_parsed(&mut self.state, "".into())?;
+                    continue;
+                }
+
+                for i in 0..instructions.len() {
+                    let res = rule.call_parsed(&mut self.state, instructions[..=i].to_string());
+                    if res.is_ok() {
+                        let _: String = instructions.drain(..i).collect();
+                        break;
+                    }
+                    if i == instructions.len() - 1 {
+                        res?;
+                    }
+                }
+            } else {
+                instructions.remove(0);
+            }
+        }
+
+        Ok(())
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,16 +3,19 @@ use crate::*;
 #[test]
 // This test taken from http://www.paulbourke.net/fractals/lsys/
 fn test_single_system_production() {
-    let mut system = LSystem::new("F+F+F+F".into(), HashMap::new());
+    let mut system = LSystem::new("F+F+F+F".into());
     system.register_production_rule("F".into(), || "F+F-F-FF+F+F-F ".into());
     system.step();
-    assert_eq!(system.axiom, "F+F-F-FF+F+F-F +F+F-F-FF+F+F-F +F+F-F-FF+F+F-F +F+F-F-FF+F+F-F ".to_owned());
+    assert_eq!(
+        system.axiom,
+        "F+F-F-FF+F+F-F +F+F-F-FF+F+F-F +F+F-F-FF+F+F-F +F+F-F-FF+F+F-F ".to_owned()
+    );
 }
 
 #[test]
 // This test taken from https://en.wikipedia.org/wiki/L-system#Example_1:_Algae
 fn test_multi_system_production() {
-    let mut system = LSystem::new("A".into(), HashMap::new());
+    let mut system = LSystem::new("A".into());
     system.register_production_rule("A".into(), || "AB".into());
     system.register_production_rule("B".into(), || "A".into());
 
@@ -23,5 +26,25 @@ fn test_multi_system_production() {
     assert_eq!(system.axiom, "ABAAB".to_owned());
 
     system.step_by(4);
-    assert_eq!(system.axiom, "ABAABABAABAABABAABABAABAABABAABAAB".to_owned());
+    assert_eq!(
+        system.axiom,
+        "ABAABABAABAABABAABABAABAABABAABAAB".to_owned()
+    );
+}
+
+#[test]
+fn test_basic_executor() {
+    type State = (i32, i32, i32);
+
+    let mut executor = LSystemExecutor::new((0, 0, 0));
+
+    executor.register_execution_rule("A".into(), |state: &mut State| state.0 += 1);
+    executor.register_execution_rule("B".into(), |state: &mut State, b: i32| state.1 += b);
+    executor.register_execution_rule("C".into(), |state: &mut State, c: i32, m: i32| {
+        state.2 += if state.0 > state.1 { c } else { m * c }
+    });
+
+    executor.execute(&LSystem::new("AAC(3, 0)B(6)B(1)AC(3, 2)".into())).unwrap();
+
+    assert_eq!(executor.state, (3, 7, 9));
 }
